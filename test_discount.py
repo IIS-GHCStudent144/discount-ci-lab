d586854c5c6042197940388895809759a5c9f18c
Student144 <IIS-GHCStudent144@innovationinsoftware.com>
2026-07-14 14:52:35 -0400

    minus 3

--- test_discount.py
+++ test_discount.py
@@ -6,13 +6,3 @@ def test_premium_discount():
 
 def test_standard_discount():
     assert calculate_discounted_price(100, "standard") == 90.00
-
-def test_negative_price_raises():
-    with pytest.raises(ValueError):
-        calculate_discounted_price(-10, "standard")
-
-def test_save10_coupon():
-    assert calculate_discounted_price(100, "standard", coupon_code="SAVE10") == 81.00
-
-def test_holiday_discount():
-    assert calculate_discounted_price(100, "guest", is_holiday=True) == 95.00
